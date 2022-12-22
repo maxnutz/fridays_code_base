d0ac7ed5b2b89b4670e49e108cb10e3b22d1d489
Max Nutz <max.nutz@gmail.com>
2022-12-22 18:01:49 +0100

    Mail, new webprogramming and start with Emission plots

--- Kraxenweg_web.py
+++ Kraxenweg_web.py
@@ -5,29 +5,60 @@ import random
 
 #web = webdriver.Chrome()
 while True:
+	fehler = 0
 	web = webdriver.Firefox()
 	print('\n Start Script')
 	
-	web.get("https://m.noen.at/krems/wegen-gutachten-hotelprojekt-in-der-kremser-innenstadt-ist-gestorben-krems-innenstadt-krems-hotelprojekt-kremser-bank-christian-hager-gutachten-print-287300483")
+	"""Website aufrufen"""
+	web.get("https://www.noen.at/niederoesterreich/chronik-gericht/protest-aktionen-noe-klimaschutz-aktivisten-strafen-nehmen-wir-in-kauf-niederoesterreich-print-klima-aktivismus-klimaschutz-klimawandel-florian-wagner-nadja-prankl-letzte-generation-346133086")
 
-	time.sleep(random.randint(4,20))
+	time.sleep(1)
 	
-	#cookies = web.find_element_by_xpath('//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]')
-	#cookies.click()
+	"""Cookies wegdrücken"""
 	
-	#time.sleep(2)
+	try:
+		cookies = web.find_element_by_xpath('/html/body/div[1]/div/div[4]/div[1]/div[2]/button[4]')
+		cookies.click()
+	except:
+		fehler += 1
+		print("Fehler beim Cookies wegdrücken.")
 	
-	gruenraum = web.find_element_by_xpath('/html/body/div/div/div[2]/div/div/div[1]/div[3]/div/div[2]/div/div[2]')
-	gruenraum.click()
+	time.sleep(2)
+	
+	"""Ja anwählen"""
+	try:
+		click_element = web.find_element_by_xpath('/html/body/div[3]/div/div[3]/article/div[3]/div/div/form/div/div/div/ul/li/div[2]/span/ul/li[1]')
+		click_element.click()
+	except Exception as e:
+		fehler +=1
+		print("Fehler beim anklicken von ja\n ", e)
+	
+	time.sleep(0.5)
+	
+	
+	"""Abschicken"""
+	try:
+		abschicken = web.find_element_by_xpath('/html/body/div[3]/div/div[3]/article/div[3]/div/div/form/div/div/div/div/a')
+		abschicken.click()
+	except:
+		fehler +=1
+		print("Fehler beim abschicken")
+	
+	if fehler > 3:
+		raise TypeError("zu viele Fehler aufgetreten. Skript hat nicht funktioniert.")
+	
 	time.sleep(random.randint(3,20))
 
 	#absenden = web.find_element_by_xpath('/html/body/div[2]/div[1]/article[1]/div[3]/form/div/div/div/div/a/span')
 	#absenden.click()
 
-	time.sleep(random.randint(5,33))
+	#time.sleep(random.randint(5,33))
 	#web.save_screenshot('screen.png')
 	web.quit()
-	s = random.randint(10,40)
-	print('\n Sleep for ')
-	print(s)
-	time.sleep(s)
+	#s = random.randint(10,40)
+	#print('\n Sleep for ')
+	#print(s)
+	time.sleep(2)
+	
+"""Cookies löschen"""
+# Chrome mit "rm ~/.config/google-chrome/Default/Cookies*"
